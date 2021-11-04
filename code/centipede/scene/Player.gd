dc460d03fcb959c661c17a55070a8f415698e9dd
Xinomik <wenkang979@gmail.com>
2021-11-03 20:11:42 -0700

    bullet fire

--- code/centipede/scene/Player.gd
+++ code/centipede/scene/Player.gd
@@ -10,32 +10,37 @@ var velocity: Vector2
 
 func _process(delta_time):
     
-    var delta_position: Vector2 = Vector2.ZERO
+    var x = Input.get_action_strength("Move_left")- Input.get_action_strength("Mover_right")
     
-    if Input.is_action_just_pressed("Move_up"):
-        delta_position = Vector2(0, -1)
-        print("move up")
-    if Input.is_action_just_pressed("Mover_down"):
-        delta_position = Vector2(0, 1)
-        print("move Down")
-    if Input.is_action_just_pressed("Move_left"): 
-        delta_position = Vector2(-1, 0)
-        print("move left")
-    if Input.is_action_just_pressed("Mover_right"):
-        delta_position = Vector2(1, 0)
-        print("move right")    
+    var y = Input.get_action_strength("Move_up") - Input.get_action_strength("Mover_down")
+    direction = Vector2( x, y)
+    
+    #var delta_position: Vector2 = Vector2.ZERO
+    
+    #if Input.is_action_just_pressed("Move_up"):
+     #   delta_position = Vector2(0, -1)
+      #  print("move up")
+    #if Input.is_action_just_pressed("Mover_down"):
+     #   delta_position = Vector2(0, 1)
+      #  print("move Down")
+    #if Input.is_action_just_pressed("Move_left"): 
+     #   delta_position = Vector2(-1, 0)
+      #  print("move left")
+    #if Input.is_action_just_pressed("Mover_right"):
+     #   delta_position = Vector2(1, 0)
+      #  print("move right")    
         
     if Input.is_action_just_pressed("fire"):
         fire()
         
        
-    Move(delta_position * speed * delta_time)
+    Move(direction * speed * delta_time)
     
 func Move (delta_position : Vector2):
       position += delta_position
 
 func fire():
-     var bullet: = BULLET_PS.instance()
+     var bullet: Bullet = BULLET_PS.instance()
      bullet.position = position
      get_parent().add_child(bullet)
      print ("fire")
